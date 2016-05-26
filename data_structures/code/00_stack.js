cede06400dcd34dd6c4fd7ba9ccc14f20d84cf43
nicholsd <nicholsd@opentext.com>
2016-05-26 15:40:27 -0400

    More Stack housekeeping

--- data_structures/code/00_stack.js
+++ data_structures/code/00_stack.js
@@ -23,30 +23,20 @@
  * 
  * Arrays in JavaScript are essentially Stacks, so this is somewhat superfluous beyond 
  * learning about how to implement a Stack.
+ * 
  */
 
 function Stack() {
-  
-  this.stack = [];
-  this.top = -1;
-  
-  this.increment = function () {
-    this.top++;
-  };
-  
-  this.decrement = function () {
-    if (this.top > -1) {
-      this.top--;  
-    }
-  };
+  this._stack = [];
+  this._top = -1;
 }
 
 Stack.prototype.push = function (item) {
-  if (item) {
-    this.increment();
-    this.stack[this.top] = item;  
+  if (typeof item === 'undefined') {
+    throw new Error('Cannot push undefined onto Stack');
   } else {
-    throw new Error('Cannot push undefined onto Stack'); 
+    this._top++;
+    this._stack[this._top] = item;
   }
 };
 
@@ -54,8 +44,8 @@ Stack.prototype.pop = function () {
   if (this.isEmpty()) {
     throw new Error('Cannot pop empty Stack');
   } else {
-    var item = this.stack[this.top];
-    this.decrement();
+    var item = this._stack[this._top];
+    this._top--;
     return item;  
   }
 };
@@ -64,10 +54,10 @@ Stack.prototype.peek = function () {
   if (this.isEmpty()) {
     throw new Error('Cannot peek empty Stack');
   } else {
-    return this.stack[this.top];
+    return this._stack[this.top];
   }
 };
 
 Stack.prototype.isEmpty = function () {
-  return this.top === -1;
+  return this._top === -1;
 };
